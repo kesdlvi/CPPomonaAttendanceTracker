ce727bf9a6298fc670e557f85e3cdf540803dd53
kesdlvi <kesdlvi12@gmail.com>
2023-07-27 23:46:52 -0700

    Added main function
    Main function added. Program currently parses the excel file and assigns points to people based on attendance.

--- main.py
+++ main.py
@@ -1,73 +1,71 @@
 import pandas as pd
 from openpyxl import load_workbook
-
-# Classes
-
 class Person: 
     def __init__ (self, name):
         self.name = name
         self.points = 0
 
-
-# Read the Excel file
-df = pd.read_excel("data.xlsx")
-
-
-# to get cell color
-wb = load_workbook("data.xlsx")
-ws = wb.active
+def main(): 
+    # Read the Excel file
+    df = pd.read_excel("data.xlsx")
 
 
-# checking if cell is green
-def did_rsvp(cell):
-    return cell.fill.fgColor.rgb == "FF00FF00"
-#"00FF0"
-
-# finding a cell location by the name in the cell
-
-def find_cell(val):
-    for row in ws.iter_rows(min_row = 1, max_row = 91, min_col = 3, max_col = 3):
-        for cell in row: 
-            if cell.value == val: 
-                return cell
-    return None
+    # to get cell color
+    wb = load_workbook("data.xlsx")
+    ws = wb.active
 
 
-# Extract the name column
-person_column_name = df.columns[2]  # Assuming the third column is at index 2 (0-based index)
-person_column_data = df[person_column_name]
-
-# Filter the non-empty cells from both columns
-non_empty_third_column = person_column_data.dropna().iloc[:91]
-
-# arrays
-attended = []
-absent = []
-
-
-# adding to arrays
-for cell in non_empty_third_column:
-    spec_cel = find_cell(cell)
-    if did_rsvp(spec_cel):
-        person = Person(cell)
-        person.points += 5
-        attended.append(person)
-    elif spec_cel == None:
-        print("DNE")
-    else:
-        abPerson = Person(cell)
-        absent.append(abPerson)
-    
-
-# Printing the arrays
-
-print("People that RSVP'ed and Attended: ")
-for person in attended:
-    print(f"{person.name}: {person.points} points")
-print() 
-print("People that were absent")
-for person in absent: 
-    print(f"{person.name}")
+    # checking if cell is green
+    def did_rsvp(cell):
+        return cell.fill.fgColor.rgb == "FF00FF00"
 
 
 
+    # finding a cell location by the name in the cell
+    def find_cell(val):
+        for row in ws.iter_rows(min_row = 1, max_row = 91, min_col = 3, max_col = 3):
+            for cell in row: 
+                if cell.value == val: 
+                    return cell
+        return None
+
+
+    # Extract the name column
+    person_column_name = df.columns[2]  # Assuming the third column is at index 2 (0-based index)
+    person_column_data = df[person_column_name]
+
+    # Filter the non-empty cells from both columns
+    non_empty_third_column = person_column_data.dropna().iloc[:91]
+
+    # Holds who attended and was absent 
+    attended = []
+    absent = []
+
+
+    # adding to arrays
+    for cell in non_empty_third_column:
+        spec_cel = find_cell(cell)
+        if did_rsvp(spec_cel):
+            person = Person(cell)
+            person.points += 5
+            attended.append(person)
+        elif spec_cel == None:
+            print("DNE")
+        else:
+            abPerson = Person(cell)
+            absent.append(abPerson)
+        
+
+    # Printing the arrays
+
+    print("People that RSVP'ed and Attended: ")
+    for person in attended:
+        print(f"{person.name}: {person.points}")
+    print() 
+    print("People that were absent")
+    for person in absent: 
+        print(f"{person.name}")
+
+
+if __name__ == "__main__":
+    main()
