07f6b0225f24c8d7e2f721702a5ab196f5990998
kesdlvi <kesdlvi12@gmail.com>
2023-07-27 21:58:21 -0700

    Update main.py
    Currently testing and printing various column cells

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 df  = pd.read_csv("data.csv")
 
+email_col_name  = df.columns[1]
+email_col_data = df[email_col_name]
+
 third_column_name = df.columns[2]  # Assuming the third column is at index 2 (0-based index)
 third_column_data = df[third_column_name ]
 
-print(third_column_data.dropna())
+print(third_column_data.dropna().iloc[:20] + " " + email_col_data.dropna().iloc[:20])
